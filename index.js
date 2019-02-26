fdcfd7c123858071bb0a52ec0ef90d5425a2fac9
jonesga1 <gabriel.jones@ucdenver.edu>
2019-02-26 16:14:02 -0700

    Change get request

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello from javascript!");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res) {
+  res.send("Hello world!")
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
